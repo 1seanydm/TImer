55ac40514641877262046ec06fa46169fa09bddc
Sean Michels <seanydm@gmail.com>
2021-11-12 08:50:35 -0700

    Completed Timer Class

--- timer.py
+++ timer.py
@@ -1,13 +1,23 @@
+from dataclasses import dataclass, field
 import time
+from typing import Callable, ClassVar, Dict, Optional
 
 
 class TimerError(Exception):
     """A custom exception used to report errors in use of Timer class"""
 
-
+@dataclass
 class Timer:
-    def __init__(self):
-        self._start_time = None
+    timers: ClassVar[Dict[str, float]] = dict()
+    name: Optional[str] = None
+    text: str = "Elapsed time: {:0.4f} seconds"
+    logger: Optional[Callable[[str], None]] = print
+    _start_time: Optional[float] = field(default=None, init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        """Add timer to dict of timers after initialization"""
+        if self.name is not None:
+            self.timers.setdefault(self.name, 0)
 
     def start(self):
         """Start a new timer"""
@@ -16,11 +26,19 @@ class Timer:
 
         self._start_time = time.perf_counter()
 
-    def stop(self):
+    def stop(self) -> float:
         """Stop the timer, and report the elapsed time."""
         if self._start_time is None:
             raise TimerError(f"Timer is not running. Use .start() to start it.")
 
+        # Calculate elapsed time
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
-        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+
+        # Report elapsed time
+        if self.logger:
+            self.logger(self.text.format(elapsed_time))
+        if self.name:
+            self.timers[self.name] += elapsed_time
+
+        return elapsed_time
